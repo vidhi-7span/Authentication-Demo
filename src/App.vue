a068ea901cf9667f317dc3363e039f9630ce3282
vidhi-7span <vidhi@7span.com>
2024-02-21 10:54:51 +0530

    Project Setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div v-for="(data, i) in detail" key="i">{{ data.name }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import details from "./static/db.js";
+
+const detail = ref(details.slice(0, 100));
+console.log(detail.value);
+</script>

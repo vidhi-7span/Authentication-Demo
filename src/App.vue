1487478ca952cf937274f0f33a823ca9f5b6a8c6
vidhi-7span <vidhi@7span.com>
2024-02-21 11:17:54 +0530

    Listing from json file

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <div>
-    <div v-for="(data, i) in detail" key="i">{{ data.name }}</div>
-  </div>
+  <home />
 </template>
 
 <script setup>
-import { ref } from "vue";
-import details from "./static/db.js";
-
-const detail = ref(details.slice(0, 100));
-console.log(detail.value);
+import home from "./pages/home.vue";
 </script>
---
+++ src/pages/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="(data, i) in detail" key="i">{{ data.name }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import details from "../static/db.js";
+
+const detail = ref(details.slice(0, 100));
+// console.log(detail.value);
+
+onMounted(async () => {
+  try {
+    const response = await fetch("../static/db.js");
+    const jsonData = await response.json();
+    detail.value = jsonData;
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+onmouse;
+</script>
